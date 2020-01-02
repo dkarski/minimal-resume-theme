5e5197a7c14f43a60f3d9c2268e02a4e9d7f72a2
Daniel Karski <daniel.karski@polidea.com>
2020-01-02 17:41:54 +0100

    Updates

---
+++ precache-manifest.79ee492fe4e1c47c65f810719997e48e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205162c59448efc362a8a00aa6470470",
+    "url": "/minimal-resume-theme/index.html"
+  },
+  {
+    "revision": "cadc2d87fadeab533d63",
+    "url": "/minimal-resume-theme/static/js/2.647062d2.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/minimal-resume-theme/static/js/2.647062d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a97ac23b044b19e16dc0",
+    "url": "/minimal-resume-theme/static/js/main.2fbe256d.chunk.js"
+  },
+  {
+    "revision": "e1889c7aff39fd71c4f3",
+    "url": "/minimal-resume-theme/static/js/runtime-main.cf4b3c80.js"
+  },
+  {
+    "revision": "013c349bbc4a018997066229a97531bb",
+    "url": "/minimal-resume-theme/static/media/profile.013c349b.png"
+  }
+]);
