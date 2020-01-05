6294b3659aee734073a7404ae274b72b40fb1509
Daniel Karski <daniel.karski@polidea.com>
2020-01-05 07:58:23 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-50c001062412352fb364.js"
+    "url": "webpack-runtime-b98349a035b15e1490ce.js"
   },
   {
     "url": "commons-a2c6443e394100fce8d5.js"
   },
   {
-    "url": "app-06e71a749c7c82372482.js"
+    "url": "app-19fd391414df94956cf1.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-c5f58849c2f4732d93c1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c77e849cb6d5855383895c40ccd46b04"
+    "revision": "32793e348dcd657210410e8ded681645"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "2795d41fe56e00772eea2a4b644a2040"
+    "revision": "24a6c4376a9a4c3637baacafe4e5a564"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -136,12 +136,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^/cv`), ``)
+  pathname = pathname.replace(new RegExp(`^/minimal-resume-theme`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/cv/app-06e71a749c7c82372482.js`))) {
+  if (!resources || !(await caches.match(`/minimal-resume-theme/app-19fd391414df94956cf1.js`))) {
     return await fetch(event.request)
   }
 
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/cv/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/minimal-resume-theme/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
